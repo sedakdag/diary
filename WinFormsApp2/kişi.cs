7f6abc14e54a13df72b6625aee98a5c7cf3674c9
sedakdag <35sedaakdag35@gmail.com>
2024-12-15 23:46:34 +0300

    project

---
+++ WinFormsApp2/kişi.cs
@@ -0,0 +1,29 @@
+﻿
+namespace WinFormsApp2
+{
+    internal class kişi
+    {
+        private int v;
+        private string kullaniciAdi;
+        private int sifre;
+        private string email;
+
+        public kişi(int v, string kullaniciAdi, int sifre, string email)
+        {
+            this.v = v;
+            this.kullaniciAdi = kullaniciAdi;
+            this.sifre = sifre;
+            this.email = email;
+        }
+
+        internal string getkulisim()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int getkulşifre()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
