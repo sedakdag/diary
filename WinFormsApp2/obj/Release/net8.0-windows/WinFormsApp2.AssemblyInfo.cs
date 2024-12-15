b57fd70c9cf420712086e5bcbbf5a7fe217a8863
zehrasila <zehrasilaozdizlekli@gmail.com>
2024-12-15 18:52:56 +0300

    push3

--- WinFormsApp2/obj/Release/net8.0-windows/WinFormsApp2.AssemblyInfo.cs
+++ WinFormsApp2/obj/Release/net8.0-windows/WinFormsApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d4bf50271c9b16d40e975a56d9370d46f78f2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c92a8eb36ca8b59a84de28eff210e53e7d0f2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
