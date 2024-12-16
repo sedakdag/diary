6b6708d8c24c8558161e54ec7efbb562a56beff4
zehrasila <zehrasilaozdizlekli@gmail.com>
2024-12-16 10:05:33 +0300

    PUSH8

---
+++ WinFormsApp2/todolist.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WinFormsApp2
+{
+    partial class todolist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.LightPink;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(30, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(294, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Merhaba , listene bir göz at..";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            groupBox1.Location = new Point(30, 86);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(216, 304);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "YAPILACAKLAR";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            groupBox2.Location = new Point(269, 86);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(216, 304);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "TAMAMLANANLAR";
+            // 
+            // todolist
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LavenderBlush;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "todolist";
+            Text = "todolist";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+    }
+}
